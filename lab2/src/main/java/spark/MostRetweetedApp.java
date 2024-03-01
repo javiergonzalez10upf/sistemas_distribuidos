6738478a92010cbe65cb8486d7922db79f850eb9
OscarColom <oscar.colom01@estudiant.upf.edu>
2024-03-01 22:08:31 +0100

    codigo final cambiado nombres variables mostretweeted

--- lab2/src/main/java/spark/MostRetweetedApp.java
+++ lab2/src/main/java/spark/MostRetweetedApp.java
@@ -1,19 +1,17 @@
 package spark;
 
-import edu.upf.model.ExtendedSimplifiedTweet;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import scala.Tuple2;
-
 import java.util.Arrays;
+import scala.Tuple2;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-
+import edu.upf.model.ExtendedSimplifiedTweet;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import static edu.upf.filter.FileLanguageFilter.filterLanguage;
 
 public class MostRetweetedApp {
@@ -25,7 +23,6 @@ public class MostRetweetedApp {
 
         String inputFiles = String.join(",", inputFilesList);
 
-
         // Create a SparkContext to initialize
         SparkConf conf = new SparkConf().setAppName("Most Retweeted Tweets for Most Retweeted Users");
         JavaSparkContext sc = new JavaSparkContext(conf);
@@ -34,39 +31,39 @@ public class MostRetweetedApp {
         JavaRDD<String> allLines = allLinesSpaces.filter(line -> !line.isEmpty());
 
         // Read from input
-        JavaRDD<ExtendedSimplifiedTweet> extendedSimplifiedTweets = allLines
+        JavaRDD<ExtendedSimplifiedTweet> ParsedExtendedSimplifiedTweets = allLines
                 .map(ExtendedSimplifiedTweet::fromJson)
                 .filter(Optional::isPresent)
                 .map(Optional::get);
 
-        // Num retweets per author (retweetedUserId, count)
-        JavaPairRDD<Long, Integer> retweetedUserCounts = extendedSimplifiedTweets
+        // Tuple2 of retweetedUserId and count
+        JavaPairRDD<Long, Integer> retweetedUserId_count = ParsedExtendedSimplifiedTweets
                 .filter(ExtendedSimplifiedTweet::isRetweeted)
                 .mapToPair(tweet -> new Tuple2<>(tweet.getRetweetedUserId(), 1))
                 .reduceByKey(Integer::sum);
 
-        // Find top 10 users based on the previous count
-        List<Tuple2<Long, Integer>> topUsers = retweetedUserCounts
+        // Top 10 users based on the previous count (Tuple2 of retweetedUserId and count )
+        List<Tuple2<Long, Integer>> top10Users = retweetedUserId_count
                 .mapToPair(tuple -> new Tuple2<>(tuple._2(), tuple._1()))
                 .sortByKey(false)
                 .mapToPair(tuple -> new Tuple2<>(tuple._2(), tuple._1()))
                 .take(10);
 
-        // Create a set of the top 10 user IDs for filtering later
-        Set<Long> topUserIds = topUsers.stream().map(Tuple2::_1).collect(Collectors.toSet());
+        // Initialise a set for the top 10 user IDs
+        Set<Long> top10UserIds = top10Users.stream().map(Tuple2::_1).collect(Collectors.toSet());
 
-        // For each user in the top 10 who has been retweeted, create a list with all their retweeted tweets
-        JavaPairRDD<Long, Iterable<ExtendedSimplifiedTweet>> tweetsGroupedByUser = extendedSimplifiedTweets
+        // List of all the retweeted tweets for each user in the previous top 10.
+        JavaPairRDD<Long, Iterable<ExtendedSimplifiedTweet>> RetweetsByUser = ParsedExtendedSimplifiedTweets
                 .filter(ExtendedSimplifiedTweet::isRetweeted)
                 .groupBy(ExtendedSimplifiedTweet::getRetweetedUserId)
-                .filter(tuple -> topUserIds.contains(tuple._1()));
+                .filter(tuple -> top10UserIds.contains(tuple._1()));
 
         // ((UserX, TweetY), CountY)
-        JavaRDD<Tuple2<Tuple2<Long, Long>, Integer>> userTweetCounts = tweetsGroupedByUser.flatMap(tuple -> {
+        JavaRDD<Tuple2<Tuple2<Long, Long>, Integer>> user_Tweet_Counts = RetweetsByUser.flatMap(tuple -> {
             Long userId = tuple._1();
             Iterable<ExtendedSimplifiedTweet> tweets = tuple._2();
 
-            List<Tuple2<Tuple2<Long, Long>, Integer>> userTweetCountList = new ArrayList<>();
+            List<Tuple2<Tuple2<Long, Long>, Integer>> user_Tweet_Count_List = new ArrayList<>();
 
             // Iterate over each tweet for the current user
             for (ExtendedSimplifiedTweet tweet : tweets) {
@@ -74,27 +71,27 @@ public class MostRetweetedApp {
                 int count = 1; // Initialize count to 1 for the current tweet
                 Tuple2<Long, Long> userTweetPair = new Tuple2<>(userId, tweetId);
                 Tuple2<Tuple2<Long, Long>, Integer> userTweetCount = new Tuple2<>(userTweetPair, count);
-                userTweetCountList.add(userTweetCount);
+                user_Tweet_Count_List.add(userTweetCount);
             }
-            return userTweetCountList.iterator();
+            return user_Tweet_Count_List.iterator();
         });
         // (userId,tweet), count)
 
-        // Convert userTweetCounts to a JavaPairRDD with ((userId, tweetId), count)
-        JavaPairRDD<Tuple2<Long, Long>, Integer> userTweetCountsPairRDD = userTweetCounts.mapToPair(tuple -> new Tuple2<>(tuple._1(), tuple._2()));
+        // Build a JavaPairRDD with ((userId, tweetId), count)
+        JavaPairRDD<Tuple2<Long, Long>, Integer> user_Tweet_Counts_PairRDD = user_Tweet_Counts.mapToPair(tuple -> new Tuple2<>(tuple._1(), tuple._2()));
 
-        // Use reduceByKey to sum counts for each (user, tweet) pair
-        JavaPairRDD<Tuple2<Long, Long>, Integer> aggregatedCounts = userTweetCountsPairRDD.reduceByKey(Integer::sum);
+        // ReduceByKey to sum counts for each (user, tweet) tuple
+        JavaPairRDD<Tuple2<Long, Long>, Integer> user_tweet = user_Tweet_Counts_PairRDD.reduceByKey(Integer::sum);
 
-        JavaPairRDD<Long, Tuple2<Long, Integer>> userTweetMaxCountPairRDD = aggregatedCounts.mapToPair(tuple -> {
+        JavaPairRDD<Long, Tuple2<Long, Integer>> user_Tweet_MaxCount_PairRDD = user_tweet.mapToPair(tuple -> {
             Tuple2<Long, Long> userTweetPair = tuple._1();
             Integer count = tuple._2();
             return new Tuple2<>(userTweetPair._1(), new Tuple2<>(userTweetPair._2(), count)); //(userId, (tweetId, count))
         });
 
-        JavaPairRDD<Long, Iterable<Tuple2<Long, Integer>>> groupedByUserRDD = userTweetMaxCountPairRDD.groupByKey();
+        JavaPairRDD<Long, Iterable<Tuple2<Long, Integer>>> groupedByUserRDD = user_Tweet_MaxCount_PairRDD.groupByKey();
 
-        // For each user id, find the tuple with the maximum count value --> (userId, (maxTweetId, count))
+        // Build the following RDD: (userId, (maxTweetId, count))
         JavaPairRDD<Long, Tuple2<Long, Integer>> mostRetweetedTweets = groupedByUserRDD.mapValues(tweetCountIterable -> {
             Tuple2<Long, Integer> maxCountTuple = null;
             int maxCount = -1;
